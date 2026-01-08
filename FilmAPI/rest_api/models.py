5bd60deae5c70a78eec9ecd53778f86076cd3874
Rubén Montero <ruben.montero.vazquez@gmail.com>
2026-01-08 10:08:48 +0100

    First commit - Backend skeleton with models.py
    We decided to drop Director as an entity, because we don't get paid
    enough

---
+++ FilmAPI/rest_api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class FilmBoxUser(models.Model):
+  username = models.CharField(unique=True, max_length=100)
+  encrypted_password = models.CharField(max_length=100)
+  # Without profile picture - let's just add a default image in the Android app
+  session_token = models.CharField(unique=True, max_length=100)
+
+class Category(models.Model):
+  title = models.CharField(max_length=100, unique=True)
+  image_url = models.CharField(max_length=200) # e.g.: http://static.com/front.jpg
+
+class Film(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=100)
+  image_url = models.CharField(max_length=200) # e.g.: http://static.com/front.jpg
+  film_url = models.CharField(max_length=200)
+  trailer_url = models.CharField(max_length=200) # e.g. https://youtube.com/watch?...
+  year = models.IntegerField()
+  length = models.IntegerField() # e.g.: 127
+  director = models.CharField(max_length=100)
+
+class CategoryFilm(models.Model):
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  film = models.ForeignKey(Film, on_delete=models.CASCADE)
+
+class FavoriteFilm(models.Model):
+  user = models.ForeignKey(FilmBoxUser, on_delete=models.CASCADE)
+  film = models.ForeignKey(Film, on_delete=models.CASCADE)
+
+class WishlistFilm(models.Model):
+  user = models.ForeignKey(FilmBoxUser, on_delete=models.CASCADE)
+  film = models.ForeignKey(Film, on_delete=models.CASCADE)
+
+class WatchedFilm(models.Model):
+  user = models.ForeignKey(FilmBoxUser, on_delete=models.CASCADE)
+  film = models.ForeignKey(Film, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+  user = models.ForeignKey(FilmBoxUser, on_delete=models.CASCADE)
+  film = models.ForeignKey(Film, on_delete=models.CASCADE)
+  content = models.CharField(max_length=200)
+  score = models.IntegerField() # 1-5
