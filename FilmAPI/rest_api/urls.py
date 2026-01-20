eaaca7921444ba206bcf0365ad78fa0f740cbaaf
Iago Donsión Fernández <idonsionf24@fpcoruna.afundacion.org>
2026-01-20 13:46:00 +0100

    get favorites

--- FilmAPI/rest_api/urls.py
+++ FilmAPI/rest_api/urls.py
@@ -11,6 +11,7 @@ from .views import (
     SearchUsersView,
     UserRegistrationView,
     LogoutView,
+    FavoriteListView,
 )
 
 urlpatterns = [
@@ -23,6 +24,7 @@ urlpatterns = [
     path("movies/<int:id>/reviews", MovieReviewView.as_view(), name="movie_review"),
 
     path("watched/<int:movie_id>", WatchedView.as_view(), name="watched"),
+    path("favorites", FavoriteListView.as_view(), name="favorite_list"),
     path("favorites/<int:movie_id>", FavoriteFilmView.as_view(), name="favorite_film"),
     path("wishlist/<int:movie_id>", WishlistFilmView.as_view(), name="wishlist"),
 
